eec6dcedf31350d2325f9fbd8e5967ce5553b796
carry <1710845733@qq.com>
2022-07-23 22:08:24 +0800

    配置vscode gdb

--- Code/99_test/c.c
+++ Code/99_test/c.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
 
+int main(int argc, char **argv)
+{
+    for(int i =0; i <10 ; ++i)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
